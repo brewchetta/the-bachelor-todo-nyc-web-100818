76dfba4f5175dd8682302c43de04856d119d7554
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 13:00:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -15,8 +15,8 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   i = 0
-  data.each do |season|
-    season.each
+  data.each do |season,v|
+    v.each do 
 end
 
 def get_occupation(data, hometown)

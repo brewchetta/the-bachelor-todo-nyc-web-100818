39c542f510f4e108a68eb9f200636cdd214633c0
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 12:58:48 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,8 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[season].each do |contestant|
+    name = contestant["name"] if contestant["occupation"] = occupation
 end
 
 def count_contestants_by_hometown(data, hometown)

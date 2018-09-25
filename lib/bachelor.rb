86503c788908fd1736e942e9d7b0048fd49d77f6
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 13:18:32 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,5 @@ def get_average_age_for_season(data, season)
       total_age += value["age"].to_i
     end
   end
-  average = total_age.to_f / total_contestants.to_f
+  total_age.to_f / total_contestants.to_f
 end
